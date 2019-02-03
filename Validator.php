b69ff69a42e64cf0f7bc49a186e8fa2403b8208f
Stoyan Drandarov <s.drundarov@abv.bg>
2019-02-03 10:52:15 +0200

    Added Validator class and basic method structure

---
+++ Validator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace library;
+
+
+Class Validator {
+
+
+    final private function __construct(){}
+
+
+    public static function execute(array $data, array $rules) {
+
+
+    }
+
+    /**
+     * @param $variable
+     * @param string $rule
+     */
+    private function validate($variable, string $rule) {
+
+    }
+
+
+    private function exists($variable) {
+
+    }
+
+    private function isString($variable) {
+
+    }
+
+    private function maxLength($variable, string $length) {
+
+    }
+
+    private function minLength($variable, string $length) {
+
+    }
+
+    private function exactLength($variable, string $length) {
+
+    }
+
+    private function symbols($variable) {
+
+    }
+
+    private function isInt($variable) {
+
+    }
+
+    private function isFloat($variable) {
+
+    }
+
+
+    private function isArray($variable) {
+
+    }
+
+    private function isBool($variable) {
+
+    }
+
+    private function isDecimal($variable) {
+
+    }
+
+    private function decimalLength($variable, string $length) {
+
+    }
+
+
+}
